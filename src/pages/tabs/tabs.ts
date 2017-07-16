975d6382c389fbc462bbf82ce601cd57bee59043
robsonklima <robsonklima@gmail.com>
2017-07-15 21:48:04 -0300

    first commit

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+import { RecipesPage } from './../recipes/recipes';
+import { ShoppingListPage } from './../shopping-list/shopping-list';
+
+@Component({
+  selector: 'page-tabs',
+  templateUrl: 'tabs.html',
+})
+export class TabsPage {
+  slPage = ShoppingListPage;
+  recipesPage = RecipesPage;
+}
